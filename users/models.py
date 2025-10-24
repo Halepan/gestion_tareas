9d9ad1318d7395dfa61787544235e33c1de1f60b
elianetmp05 <elianetmp05>
2025-10-24 02:12:11 -0400

    se refactorizo el repositorio

--- users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Tareas(models.Model):
-    users = models.ForeignKey(User, on_delete=models.CASCADE)
-    nombre = models.CharField(max_length=300, blank=True)
-    estado = models.BooleanField(default=False)
-    descripcion = models.CharField(max_length=999999)
